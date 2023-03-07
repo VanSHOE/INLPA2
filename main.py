cf40aa018bb47710b236732d5e3fedb5edc319bf
Rahul Garg <rahulgargofficial@gmail.com>
2023-03-07 14:30:59 +0530

    data set prep

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import torch
 import torchtext
-
+import numpy as np
+from pprint import pprint
 import conllu
 
 sentenceLens = {}
@@ -29,14 +30,16 @@ class Data(torch.utils.data.Dataset):
             self.sentences.append(sentence[:half])
             self.sentences.append(sentence[half:])
         self.sentences += self.Ssentences
-        print(self.sentences)
-        exit(1)
-        self.sentences = [["<bos>"] + sentence + ["<eos>"] for sentence in self.sentences if
+        # print(self.sentences)
+
+        self.sentences = [[("<bos>", "<bot>")] + sentence + [("<eos>", "<eot>")] for sentence in self.sentences if
                           len(sentence) <= self.cutoff]
 
         # print(self.sentences)
-        print(len(self.sentences))
+        # print(len(self.sentences))
+        # exit(1)
         self.vocab = set()
+        self.tagVocab = set()
         self.mxSentSize = 0
         # self.mxSentSize = 20
         for sentence in self.sentences:
@@ -45,50 +48,72 @@ class Data(torch.utils.data.Dataset):
             else:
                 sentenceLens[len(sentence)] += 1
             for token in sentence:
-                self.vocab.add(token)
+                self.vocab.add(token[0])
+                self.tagVocab.add(token[1])
 
             if len(sentence) > self.mxSentSize:
                 self.mxSentSize = len(sentence)
 
         self.vocab = list(self.vocab)
+        self.tagVocab = list(self.tagVocab)
 
         # add padding token
         self.vocab.append("<pad>")
+        self.tagVocab.append("<pad>")
         # add Unknown
         if "<unk>" not in self.vocab:
             self.vocab.append("<unk>")
 
+        if "<unk>" not in self.tagVocab:
+            self.tagVocab.append("<unk>")
+
         if "<bos>" not in self.vocab:
             self.vocab.append("<bos>")
 
+        if "<bot>" not in self.tagVocab:
+            self.tagVocab.append("<bot>")
+
         if "<eos>" not in self.vocab:
             self.vocab.append("<eos>")
+
+        if "<eot>" not in self.tagVocab:
+            self.tagVocab.append("<eot>")
 
         self.vocabSet = set(self.vocab)
         self.w2idx = {w: i for i, w in enumerate(self.vocab)}
         self.idx2w = {i: w for i, w in enumerate(self.vocab)}
 
+        # print(self.idx2w)
+
+        self.tagVocabSet = set(self.tagVocab)
+        self.tagW2idx = {w: i for i, w in enumerate(self.tagVocab)}
+        self.tagIdx2w = {i: w for i, w in enumerate(self.tagVocab)}
+
         # pad each sentence to 40
         for i in range(len(self.sentences)):
             # print(type(self.sentences[i]))
-            self.sentences[i] = self.sentences[i] + ["<pad>"] * (self.mxSentSize - len(self.sentences[i]))
-        self.sentencesIdx = torch.tensor([[self.w2idx[token] for token in sentence] for sentence in self.sentences],
+            self.sentences[i] = self.sentences[i] + [("<pad>", "<pad>")] * (self.mxSentSize - len(self.sentences[i]))
+        self.sentencesIdx = torch.tensor([[self.w2idx[token[0]] for token in sentence] for sentence in self.sentences],
                                          device=self.device)
+        self.tagSentencesIdx = torch.tensor(
+            [[self.tagW2idx[token[1]] for token in sentence] for sentence in self.sentences],
+            device=self.device)
         self.padIdx = self.w2idx["<pad>"]
+        self.tagPadIdx = self.tagW2idx["<pad>"]
 
     def handle_unknowns(self, vocab_set, vocab):
         for i in range(len(self.sentences)):
             for j in range(len(self.sentences[i])):
-                if self.sentences[i][j] not in vocab_set:
+                if self.sentences[i][j][0] not in vocab_set:
                     # remove from vocab and vocab set
-                    if self.sentences[i][j] in self.vocab:
-                        self.vocab.remove(self.sentences[i][j])
-                    if self.sentences[i][j] in self.vocabSet:
-                        self.vocabSet.remove(self.sentences[i][j])
+                    if self.sentences[i][j][0] in self.vocab:
+                        self.vocab.remove(self.sentences[i][j][0])
+                    if self.sentences[i][j][0] in self.vocabSet:
+                        self.vocabSet.remove(self.sentences[i][j][0])
                     self.sentences[i][j] = "<unk>"
         self.w2idx = {w: i for i, w in enumerate(vocab)}
         self.idx2w = {i: w for i, w in enumerate(vocab)}
-        self.sentencesIdx = torch.tensor([[self.w2idx[token] for token in sentence] for sentence in self.sentences],
+        self.sentencesIdx = torch.tensor([[self.w2idx[token[0]] for token in sentence] for sentence in self.sentences],
                                          device=self.device)
 
     def __len__(self):
@@ -96,7 +121,7 @@ class Data(torch.utils.data.Dataset):
 
     def __getitem__(self, idx):
         # sentence, last word
-        return self.sentencesIdx[idx],
+        return self.sentencesIdx[idx], self.tagSentencesIdx[idx]
 
 
 input = open('./UD_English-Atis/en_atis-ud-train.conllu', 'r', encoding='utf-8')
@@ -114,3 +139,7 @@ for sentence in data:
     sentences.append(unitSentence)
 
 trainData = Data(sentences, tags)
+for sentence, tag in trainData:
+    # convert back to word from idx
+    s = [(trainData.idx2w[int(idx)], trainData.tagIdx2w[int(tag)]) for idx, tag in zip(sentence, tag)]
+    pprint(s)
